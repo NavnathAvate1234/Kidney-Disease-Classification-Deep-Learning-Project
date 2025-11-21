fcd56f1200284a8809f847f93447a947b4aed9a4
AwateNavnath <awatenavnath39@gmail.com>
2025-11-21 16:57:06 +0530

    workflows add

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-import setuptools 
+import setuptools
 
-with open("README.md","r",encoding = "urf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
@@ -9,22 +9,21 @@ __version__ = "0.0.0"
 REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
 AUTHOR_USER_NAME = "NavnathAvate1234"
 SRC_REPO = "cnnClassifier"
-AUTHOP_EMAIL = "awatenavnath39@gmail.com"
+AUTHOR_EMAIL = "awatenavanth39@gmail.com"
+
 
 setuptools.setup(
-    name = SRC_REPO,
-    version = __version__,
-    author = AUTHOR_USER_NAME,
-    author_email = AUTHOR_EMAIL,
-    description= "A small python package for CNN app",
-    Long_description = long_description,
-    Long_description_content = "text/markdown",
-    url= f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls = {
-        "Bug Tracker" : f"https://github.com/{AUTHEOR_USER_NAME}/{REPO_NAME}/issues",
-
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="src")
-    
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
